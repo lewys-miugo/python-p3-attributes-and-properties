8a73aeaabbf09f50bad0ff4452ed164fe6cbd673
lewys-miugo <lewiswambugu01@gmail.com>
2025-08-16 14:56:49 +0300

    Building properties and setters

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,35 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="Fido", breed="Pug"):
+        self._name = "Fido"
+        self.name = name
+
+        self._breed ="Pug"
+        self.breed = breed
+
+
+        
+    @property
+    def name(self):
+        return self._name
+        
+    @name.setter
+    def name(self, dog_name):
+        if isinstance(dog_name, str) and 1<= len(dog_name) <=25:
+            self._name=dog_name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+    # pass
+    # pass
+
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, dog_breed):
+        if dog_breed in APPROVED_BREEDS:
+            self._breed = dog_breed
+        else:
+            print("Breed must be in list of approved breeds.")
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,33 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name = "Lewis", job="Sales"):
+        self._name = "Lewis"
+        self.name=name
+
+        self._job = "Sales"
+        self.job=job
+
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, person_name):
+        if isinstance(person_name,str) and 1<= len(person_name) <=25:
+            self._name = person_name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    @property
+    def job(self):
+        return self._job
+    
+    @job.setter
+
+    def job(self, job):
+        if job in APPROVED_JOBS:
+            self._job = job
+        else:
+            print("Job must be in list of approved jobs.")
